f3e4c1c1fe9205ba9389ff6705564464716e501f
suma511 <eliansumaia@gmail.com>
2022-01-24 02:01:06 +0200

    exam

---
+++ challenge06/Challenge06.js
@@ -0,0 +1,57 @@
+'use strict';
+
+// 1) ---------------------
+// 
+//  using the (findMax) function; return the maximum value in the provided array
+//  without using build-in functions.
+//  EX:
+//  [12, 32, 22, 45, 78, 12, 50] ==> 78
+// 
+// ------------------------
+
+const findMax = (arr)=>{
+    let max;
+    for ( let i = 0; i < Array.length; i++ ) {
+        if(arr[i]>=max) {
+            max = arr[i];
+            console.log(max);
+        }
+    }
+    return max;
+}
+
+// 2) ---------------------
+// 
+//  using the (sumNums) function; return the Sum value of the numeric values
+//  inside the array (there could be strings inside), without using build-in functions
+//  EX:
+//  [20, '234', 'car', 41, 20, 'chair'] ==> 81
+//  
+// ------------------------
+
+const sumNums = (arr)=>{
+    let sum;
+    for( let i = 0; i < 6; i++ ){
+        if(arr[i] != Number){
+        continue ; }
+        sum = sum + arr[i] ;
+
+    return sum;
+}
+
+// 3) ---------------------
+// 
+//  using the (reverseArray) function; return provided array reversed
+//  without using build-in functions
+//  EX:
+//  ['C#', 'JS', 'Ruby','Python' ] ==> ['Python','Ruby','JS','C#']
+// 
+// ------------------------
+
+const reverseArray = (arr)=>{
+    for(let i = 3; i >= 0 ; i--){
+        console.log(arr[i])
+    }
+}
+
+module.exports = {findMax , sumNums, reverseArray};
